05bc49ff45824eedf1c4fb4eb8245826d9f31c46
DXinter <58430974+DXinter@users.noreply.github.com>
2019-12-09 19:24:00 +0300

    Add files via upload

---
+++ Case10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Case10
+{
+    class Case10
+    {
+        /* 
+         * extension method для строки IsUpperCase, который опеределяет все ли символы в строке с большой буквы:
+         "hello I AM DONALD".IsUpperCase() == false
+         "HELLO I AM DONALD".IsUpperCase() == true
+         "ACSKLDFJSgSKLDFJSKLDFJ".IsUpperCase() == false
+         "ACSKLDFJSGSKLDFJSKLDFJ".IsUpperCase() == true
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string input = Console.ReadLine();       
+            bool r = input.IsUpperCase();        
+            Console.WriteLine($"{r}");
+            
+        }
+        
+    }
+}
---
+++ Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Case10
+{
+    public static class Helper
+    {
+        public static bool IsUpperCase(this string direct)
+        {
+            for (int i = 0; i < direct.Length; i++)
+            {
+                if (Char.IsLower(direct[i]))
+                    return false;
+            }
+            return true;
+        }
+       
+}   }
